c8b6fcc08a311f9ebdcd520e1c87cff4f1787716
Carolyn Ndavi <cn.ndavi@hotmail.com>
2022-01-14 08:41:53 +0100

    update stopwatch

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
-('use strict');
+'use strict'
 
-const stopwatch = document.querySelector('.stopwatch');
-const startButton = document.querySelector('#play-btn');
-const pauseButton = document.querySelector('#pause-btn');
-const stopButton = document.querySelector('#stop-btn');
+const stopwatch = document.querySelector('.stopwatch')
+const startButton = document.querySelector('#play-btn')
+const pauseButton = document.querySelector('#pause-btn')
+const stopButton = document.querySelector('#stop-btn')
 
-let milliseconds = 0;
-let seconds = 0;
-let minutes = 0;
-let timer;
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+let timer
 
 function startTimer() {
   if (!timer) {
-    timer = setInterval(runTimer, 10);
+    timer = setInterval(runTimer, 10)
   }
 }
 
 function runTimer() {
-  getTimer();
-  milliseconds++;
+  getTimer()
+  milliseconds++
   if (milliseconds == 100) {
-    milliseconds = 0;
-    seconds++;
+    milliseconds = 0
+    seconds++
   }
   if (seconds == 60) {
-    seconds = 0;
-    minutes++;
+    seconds = 0
+    minutes++
   }
 }
 
 function pauseTimer() {
-  stopTimer();
+  stopTimer()
 }
 
 function resetTimer() {
-  stopTimer();
-  milliseconds = 0;
-  seconds = 0;
-  minutes = 0;
-  getTimer();
+  stopTimer()
+  milliseconds = 0
+  seconds = 0
+  minutes = 0
+  getTimer()
 }
 
 function stopTimer() {
-  clearInterval(timer);
-  timer = false;
+  clearInterval(timer)
+  timer = false
 }
 
 function getTimer() {
@@ -52,11 +52,11 @@ function getTimer() {
     ':' +
     (seconds < 10 ? '0' + seconds : seconds) +
     ':' +
-    (milliseconds < 10 ? '0' + milliseconds : milliseconds));
+    (milliseconds < 10 ? '0' + milliseconds : milliseconds))
 }
 
-startButton.addEventListener('click', startTimer);
-pauseButton.addEventListener('click', stopTimer);
-stopButton.addEventListener('click', resetTimer);
+startButton.addEventListener('click', startTimer)
+pauseButton.addEventListener('click', stopTimer)
+stopButton.addEventListener('click', resetTimer)
 
-feather.replace();
+feather.replace()
